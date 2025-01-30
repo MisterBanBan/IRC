32851e21ef17cb926ae244e0ba9c2fe2caedc511
zigeuh <zigeuh13@gmail.com>
2025-01-30 15:15:10 +0100

    - Flag verifications for mode - Moved all public attributes from Channel to private - Moved isOperator function from Server to Channel since it is for channels - Created getter and setter for private attributes of Channel and changed everywhere needed - Started to read all the input (in case there are seperated flags)

--- inc/Channel.hpp
+++ inc/Channel.hpp
@@ -6,7 +6,7 @@
 /*   By: mbaron-t <mbaron-t@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/29 09:14:22 by mbaron-t          #+#    #+#             */
-/*   Updated: 2025/01/29 09:14:26 by mbaron-t         ###   ########.fr       */
+/*   Updated: 2025/01/30 14:39:35 by mbaron-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,47 @@ class Channel
         const std::set<int>& getMembers() const;
         void addMember(int fd);
         void removeMember(int fd);
+		void inviteUser(int fd);
+
         bool isMember(int fd) const;
-        void inviteUser(int fd);
-        bool isInvited(int fd);
+        bool isInvited(int fd) const;
+		bool isOperator(int fd) const;
+
         std::string getTopic() const;
 		std::string getName() const;
-        void setTopic(const std::string &topic);
-        bool inviteOnly; //+i
-        bool topicLocked; //+t
-        bool hasKey; //+k
-        std::string key;
-        bool limitUser; //+l
-        int userLimit;
+		std::string	getKey() const;
+		int			getUserLimit() const;
 
-        std::set<int> operators; //+o
+		std::set<int> & getOperators();
+
+		bool getHasKey() const;
+		bool getTopicLocked() const;
+		bool getInviteOnly() const;
+		bool getLimitUser() const;
+
+		void setTopicLocked(bool active);
+		void setHasKey(bool active);
+		void setInviteOnly(bool active);
+		void setLimitUser(bool active);
+
+		void setUserLimit(int limit);
+		void setKey(const std::string & key);
+		void setTopic(const std::string &topic);
         
     private:
         std::string _name;
         std::set<int> _members;
         std::string _topics;
         std::set<int> _inviteUser;
+
+		bool		_inviteOnly; //+i
+		bool		_topicLocked; //+t
+		bool		_hasKey; //+k
+		std::string	_key;
+		bool		_limitUser; //+l
+		int			_userLimit;
+
+		std::set<int> _operators; //+o
 };
 
 #endif
