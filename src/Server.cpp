cb05ebbf9080731e304dfbb6070dcfb456e6c9bc
MisterBanBan <favier.alban@gmail.com>
2025-02-19 16:37:29 +0100

    remove comment

--- src/Server.cpp
+++ src/Server.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arvoyer <arvoyer@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mtbanban <mtbanban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 12:43:53 by mbaron-t          #+#    #+#             */
-/*   Updated: 2025/02/17 13:25:26 by mbaron-t         ###   ########.fr       */
+/*   Updated: 2025/02/19 16:36:39 by mtbanban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,6 @@ int	Server::initServerSocket(const std::string & port, const std::string & pass)
     }
 
 	this->_serverHashPassword = crypt(pass.c_str(), "$6$RNEuivJ08k");
-    //It creates a socket using the protocol IPV4 and a socket orient connexion SOCK_STREAM
-    //Concretely, socket() returns a file descriptor (an integer) which identifies this new socket
-    //listen on one port
     _server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (_server_fd < 0)
     {
@@ -62,61 +59,35 @@ int	Server::initServerSocket(const std::string & port, const std::string & pass)
         return 1;
     }
     int opt = 1;
-    //An option (SO_REUSEADDR) is defined at the socket level (SOL_SOCKET).
-    //SO_REUSEADDR allows reuse of address and port immediately after restarting the server
-    //opt is used to enable or disable the option 0 disable 1 enable
     if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR ,&opt, sizeof(opt)) == -1)
 	{
 		std::cout << "Error: " << errno << std::endl;
 		return 1;
 	}
-     /*struct sockaddr_in {
-    sa_family_t    sin_family;   // Adress Familly (AF_INET for IPv4)
-    in_port_t      sin_port;     // The port (in network format)
-    struct in_addr sin_addr;     // Adress IPv4
-    unsigned char  sin_zero[8];  // Zero-filled, not generally used*/
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr)); //filling with zeros
-    serv_addr.sin_family = AF_INET; //to use familly adress IPV4
-    //serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //INADDR_ANY correspond to 0.0.0.0 the server will listen on all available network interfaces convert from host format to network format (little-endian" (the least significant byte is stored first)) big-endian » (high-order byte is stored first) TCP/IP “network byte order”, which is big-endian.
-    /*if (ip == "0.0.0.0")
-    {
-        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    } 
-    else
-    {
-        if (inet_pton(AF_INET, ip.c_str(), &serv_addr.sin_addr) <= 0)
-        {
-            std::cerr << "Error: Invalid IP address (" << ip << ")" << std::endl;
-            close(_server_fd);
-            exit(EXIT_FAILURE);
-        }
-    }*/
-    serv_addr.sin_port = htons(portInt); //the same thing but is to 16 bits
-    // bind "attach" the socket to a port on the machine a socket has no address or port defined, and therefore cannot receive connections
-    if (bind(_server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portInt);
+   if (bind(_server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         std::cout << "Error: bind failed (" << strerror(errno) << ")" << std::endl;
         return 1;
     }
-    //SOMAXCONN allows to define the max possible connexion is generally 128 
     if (listen(_server_fd, SOMAXCONN))
     {
         std::cout << "Error: listen failed" << std::endl;
         return 1;
     }
-    // manage multiple connections without blocking
-    if (fcntl(_server_fd, F_SETFL, O_NONBLOCK) < 0) // F_SETFL define a new flags for the socket activate noBLOCK
+    if (fcntl(_server_fd, F_SETFL, O_NONBLOCK) < 0)
     {
         std::cout << "Error: F_SETFL failed" << std::endl;
         return 1;
     }
-    // _poll_fds is used to store all file descriptors (sockets) that the server wants to monitor for events.
     struct pollfd server_pollfd;
-    server_pollfd.fd = _server_fd; //Assigns the server socket file descriptor (_server_fd) to the fd field of the pollfd structure.
-    server_pollfd.events = POLLIN; // Sets the POLLIN event type: Indicates that we are monitoring read events.
-    server_pollfd.revents = 0; //This field is used by poll() to indicate which events actually occurred.
-    _poll_fds.push_back(server_pollfd); //This allows the server to detect when a new connection is ready to be accepted
+    server_pollfd.fd = _server_fd;
+    server_pollfd.events = POLLIN;
+    server_pollfd.revents = 0;
+    _poll_fds.push_back(server_pollfd);
 	return 0;
 }
 
@@ -124,11 +95,9 @@ void Server::acceptNewClient()
 {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
-    // _server_fd The file descriptor of the server socket that was initially created with socket() and made to listen with bind() and listen()
-    // A pointer to a sockaddr structure where accept() will store the client's address.
     int client_fd = accept(_server_fd, (struct sockaddr *)&client_addr, &client_len);
     if (client_fd < 0) 
-    {   // EWOULDBLOCK and EAGAIN are error codes that indicate that the operation should have been blocking, but the socket is in non-blocking mode and no connections were ready to be accepted at the time of the call.
+    {
         if (errno != EWOULDBLOCK && errno != EAGAIN)
         {
             std::cout << "Error: accept failed (" << strerror(errno) << ")" << std::endl;
@@ -160,9 +129,7 @@ void Server::run()
 
     while (_running)
     {
-        //The poll() function monitors a set of file descriptors for events
-        //_poll_fds.data() to the first element of the _poll_fds vector, which contains the pollfd structures describing the file descriptors to monitor
-        int ret = poll(_poll_fds.data(), _poll_fds.size(), -1); // Timeout infini if ret < 0 error
+        int ret = poll(_poll_fds.data(), _poll_fds.size(), -1);
         if (ret < 0 && _running)
         {
             std::cout << "Error: poll failed" << std::endl;
@@ -172,10 +139,8 @@ void Server::run()
         {
             int fd = _poll_fds[i].fd;
             short revents = _poll_fds[i].revents;
-            // If errors or a disconnection (POLLERR, POLLHUP, POLLNVAL) are detected, the client is removed (which also removes the _poll_fds descriptor).
             if (revents & (POLLERR | POLLHUP | POLLNVAL))
             {
-                //As _poll_fds has been modified, we do not do any incrementation here
                 removeClient(fd);
                 continue;
             }
@@ -186,7 +151,6 @@ void Server::run()
                 else 
                     clientData(fd);
             }
-            //If the write event (POLLOUT) is detected, we call the handleWriteEvent function to send the pending data to the buffer.
             if (revents & POLLOUT)
                 handleWriteEvent(fd);
             ++i;
@@ -435,16 +399,12 @@ void Server::sendToClient(int client_fd, const std::string &response)
 void Server::handleWriteEvent(int client_fd)
 {
     Client &client = _clients[client_fd];
-    //If the output buffer is empty, there is nothing to send.
-    //We then deactivate the POLLOUT flag for this client.
     if (client.getBufferOut().empty())
     {
         disableWriteEvent(client_fd);
         return;
     }
-    //We attempt to send the contents of the output buffer to the socket.
     ssize_t bytes_sent = send(client_fd, client.getBufferOut().c_str(), client.getBufferOut().size(), 0);
-    //If the sending was successful and a few bytes were transmitted
     if (bytes_sent > 0)
     {
         client.getBufferOut().erase(0, bytes_sent);
@@ -453,13 +413,11 @@ void Server::handleWriteEvent(int client_fd)
     }
     else if (bytes_sent == -1)
     {
-        //If the error is not EAGAIN or EWOULDBLOCK,This is a critical error and we delete the client.
         if (errno != EAGAIN && errno != EWOULDBLOCK)
         {
             std::cerr << "Error: send failed to FD " << client_fd << " (" << strerror(errno) << ")" << std::endl;
             removeClient(client_fd);
         }
-        //Otherwise, EAGAIN or EWOULDBLOCK simply means that the socket is not ready to write at the moment, and we will wait for the next POLLOUT.
     }
 }
 
